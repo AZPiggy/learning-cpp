54d02aec5b7025524a761cada5a13afafb017886
Angie <angela.yanqizhu@gmail.com>
2024-07-08 00:38:27 +0000

    console interaction

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -6,9 +6,11 @@
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string str; // declare the variable str (string type)
+    // arrow opposite to cout
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> str; // keyboard input is stored into str
+    std::cout << "Hello, " << str << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
