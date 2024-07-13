6792ccc24cd2ef1aa0a41f827392d4912dea5f0c
Angie <angela.yanqizhu@gmail.com>
2024-07-13 16:08:23 +0000

    Array

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -4,7 +4,20 @@
 
 #include <iostream>
 
-int main(){
+int main()
+{
+    int age[4];
+    float temperature[] = {31.5, 32.7, 38.9};
+
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
+
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << "The age " << i << " is " << age[i] << std::endl;
+    }
     
     std::cout << std::endl << std::endl;
     return (0);
