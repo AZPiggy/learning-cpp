353ecebc33e92b30e63abb57a593503f26d3ec84
Angie <angela.yanqizhu@gmail.com>
2024-07-15 02:22:34 +0000

    More casting

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,6 +6,21 @@
 #include <cstdint>
 
 int main(){
+    int fahrenheit = 100;
+    int celsius;
+
+    celsius = ((float)5 / 9.0) * (fahrenheit - 32); // if integer, will perform integer division and get an error
+
+    std::cout << std::endl;
+    std::cout << "Fahrenheit: " << fahrenheit << std::endl;
+    std::cout << "Celsius   : " << celsius << std::endl;
+
+    float weight = 10.99;
+
+    std::cout << std::endl;
+    std::cout << "Float:           " << weight << std::endl;
+    std::cout << "Integer Part:    " << (int)weight << std::endl;
+    std::cout << "Fractional Part: " << (int)((weight - (int)weight) * 10000) << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
