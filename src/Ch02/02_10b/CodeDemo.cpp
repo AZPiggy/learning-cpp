36e80b2a6aa288e73dbbe6009cdcfeaaab324f49
Angie <angela.yanqizhu@gmail.com>
2024-07-15 02:12:34 +0000

    Type casting intro

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -7,6 +7,19 @@
 
 int main(){
     
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    // implicit type conversion
+    flt = -7.66; // double constant
+    sgn = flt; // will be -7
+    unsgn = sgn; // two's complement
+
+    std::cout << " float: " << flt << std::endl;
+    std::cout << " int32: " << sgn << std::endl;
+    std::cout << "uint32: " << (int32_t) unsgn << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
