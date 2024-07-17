7d5a118fbb22e7d5ffa3c6a0b4d1a2e46073b617
Angie <angela.yanqizhu@gmail.com>
2024-07-17 00:02:00 +0000

    Exercise

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -6,10 +6,15 @@
 
 int main(){
     int nums[5] = {1,23,32,24,337};
-    float result; 
+    float result = 0; 
 
-    // Write your code here
-    
+    for (int i = 0; i <= 4; i++)
+    {
+        result += (float)nums[i];
+    }
+
+    result = (float)result/5;
+
     std::cout << "The average is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
