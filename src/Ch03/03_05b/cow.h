4e19cf685ce478193e9b72b565f234dc5e8d6668
Angie <angela.yanqizhu@gmail.com>
2024-07-22 00:34:05 +0000

    multiple source files

---
+++ src/Ch03/03_05b/cow.h
@@ -0,0 +1,23 @@
+// telling the compiler to only include this header file once
+#pragma once
+
+// an alternative
+#ifndef COW_h
+#define COW_h
+#include <string>
+
+enum class cow_purpose {dairy, meat, hide, pet};
+
+class cow{
+public:
+    cow(std::string name_i, int age_i, cow_purpose purpose_i);
+    std::string get_name() const;
+    int get_age() const;
+    cow_purpose get_purpose() const;
+    void set_age(int new_age);
+private:
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+#endif // COW_h
