565c87ff7bdf5a54ee9ffe3a49f4f34ca24eab0a
Angie <angela.yanqizhu@gmail.com>
2024-08-07 18:34:04 +0000

    While and do while loops

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -8,7 +8,21 @@
 int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     
-    std::vector<int>::iterator ptr = numbers.begin();
+    auto ptr = numbers.begin(); // real type: std::vector<int>::interator
+
+    while (ptr != numbers.end())
+    {
+        std::cout << *ptr << " ";
+        ptr = next(ptr,1);
+    }
+    std::cout << std::endl << std::endl;
+    
+    // When you are sure that the loop body is safe:
+    int i = 0;
+    do{
+        std::cout << numbers[i] << " ";
+        i++;
+    } while (i < numbers.size());
     
     std::cout << std::endl << std::endl;
     return (0);
