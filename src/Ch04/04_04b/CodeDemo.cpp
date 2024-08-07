e9c46ca0782e8a20c77ea614be4d151d059978e7
Angie <angela.yanqizhu@gmail.com>
2024-08-07 19:24:41 +0000

    for loops

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -8,6 +8,24 @@
 int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     float average;
+
+    average = 0.0f;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        average += numbers[i];
+    }
+    average /= numbers.size();
+    std::cout << "Average: " << average << std::endl;
+
+    average = 0.0f;
+    // x is the variable used to traverse the container (for each "x" in "numbers")
+    for (auto x : numbers)
+    {
+        average += x;
+    }
+    average /= numbers.size();
+    std::cout << "Average: " << average << std::endl;
+    
     
     std::cout << std::endl << std::endl;
     return (0);
